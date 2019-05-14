afee4eb27310573f8adde6083c74240ca8cc909c
Gustavo Coelho <gutorc@hotmail.com>
2019-05-14 00:59:31 -0300

    Url config
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

--- src/api.js
+++ src/api.js
@@ -1,16 +1,33 @@
 import axios from 'axios'
-const instance = axios.create({
+
+let instance = axios.create({
     baseURL: 'localhost:8000',
     headers: {
         'Authorization': 'Basic YWRtaW46YWRtaW4=',
     }
 })
 
-instance.prototype.getUsers = async () => {
-    try {
-        return await instance.get('user')
-    } catch (err) {
-        return err
+let urls = [
+    {
+        'url': 'user',
+        'methods': ['GET']
+    }
+]
+
+let obj = {}
+let allMethods = ['GET', 'POST', 'PUT', 'DELETE']
+for (let config of urls) {
+    let methods = 'methods' in config ? config.methods : allMethods
+    for (let method of methods) {
+        let methodName = method.toLowerCase() + config.url
+        obj[methodName] = async () => {
+            try {
+                return await instance.get(config.url)
+            } catch (err) {
+                return err
+            }
+        }
     }
 }
-export default instance
+
+export default obj
--- test/test.user.spec.js
+++ test/test.user.spec.js
@@ -1,9 +1,13 @@
 import assert from 'assert'
+import polyfill from "@babel/polyfill"
 import api from '../src/api'
 
 describe('#find()', function() {
     it('respond with matching records', async function() {
-      let response = await api.getUsers()
+      for (let key of Object.keys(api)) {
+        console.log('api', key)
+      }
+      let response = await api.getuser()
       assert(response !== null)
     });
 });
