11b71d2b973d8438d9d9138b881687309ad86b16
gutorc92 <gutorc@hotmail.com>
2019-05-31 19:50:49 -0300

    Get with parameters
    Signed-off-by: gutorc92 <gutorc@hotmail.com>

---
+++ src/mongoBuilder.js
@@ -0,0 +1,96 @@
+/**
+* QueryBuilder - Parse query json object given into a query string
+* @param {object} queryObject - Json to be converted to query string
+* @return {string} - converted query string
+*/
+function QueryBuilder (queryObject) {
+    queryObject = JSON.parse(JSON.stringify(queryObject))
+    console.log('query object', queryObject.email)
+    filterEmptyPropsFromJson(queryObject)
+    let query = jsonToQueryString(queryObject)
+    console.log('final query', query)
+    return query
+  }
+  
+  /**
+  * filterJson - removes Json atributes that match checker Function
+  * @param {object} json - json to be filtered
+  * @return {object} - filtered object
+  */
+  const filterEmptyPropsFromJson = (json) => {
+    for (let prop in json) {
+      if (json.hasOwnProperty(prop)) {
+        if (json[prop] === '' || json[prop] === null || json[prop] === undefined) {
+          delete json[prop]
+        } else {
+          if (typeof json[prop] === 'object') { filterEmptyPropsFromJson(json[prop]) }
+        }
+      }
+    }
+  }
+  
+  /**
+  * jsonToQueryString - Takes a javascript object and parses it to a mongo query string
+  * @param {object} jsonObj - object to be converted to query string
+  * @return {string} - parsed mongo query string
+  */
+  const jsonToQueryString = (jsonObj) => {
+    return '{' + recursiveJSONParse('', '', jsonObj).replace(',', '') + '}'
+  }
+  
+  /**
+  * recursiveJSONParse - general parse method for jsons both nested and unested into mongo query strings
+  * @param {string} path - path to current node
+  * @param {string} nodName - current node name
+  * @param {value} nodContent - current node content. Can be anything but null or undefined
+  * @return {string} - returns current node's proper mongo query string
+  */
+  function recursiveJSONParse (path, nodName, nodContent) {
+    let finalStr = ''
+    path += (path.length > 0) ? '.' + nodName : nodName
+    console.log('path', path)
+    if (typeof nodContent !== 'object') {
+      return ',%22' + path + '%22:' + putDoubleQuotesIfString(nodContent)
+    }
+    console.log('passou aqui')
+  
+    for (let prop in nodContent) {
+      if (prop[0] === '$') return nonRecursiveJSONParse(path, nodContent)
+      finalStr += recursiveJSONParse(path, prop, nodContent[prop])
+    }
+  
+    return finalStr
+  }
+  
+  /**
+  * nonRecursiveJSONParse - particular parse method for objects with mongo
+  * operator properties such as $gte
+  * @param {string} path - path to current object
+  * @param {object} jsonObj - json object with mongo operator props to be parsed
+  * @return {string} - jsonObj's proper mongo query string
+  */
+  function nonRecursiveJSONParse (path, jsonObj) {
+    let str = ':{'
+  
+    for (let prop in jsonObj) {
+      str += ',%22' + prop + '%22:' + putDoubleQuotesIfString(jsonObj[prop])
+    }
+  
+    return ',%22' + path + '%22' + str.replace(',', '') + '}'
+  }
+  
+  /**
+  * putDoubleQuotesIfString - Puts double quotes on given param if param is a string
+  * else returns param itself
+  * @param {value} param - param to be type checked and double quoted if it is a string
+  * @return {value} - param's value if not a string or double quoted if param is a string
+  */
+  function putDoubleQuotesIfString (param) {
+    if (typeof param === 'string') {
+      return '%22' + param + '%22'
+    }
+    return param
+  }
+  
+  export default QueryBuilder
+  
--- test/test.user.spec.js
+++ test/test.user.spec.js
@@ -2,12 +2,42 @@ import assert from 'assert'
 import polyfill from "@babel/polyfill"
 import api from '../src/api'
 
-describe('#find()', function() {
+describe('#find users ()', function() {
     it('respond with matching records', async function() {
-      for (let key of Object.keys(api)) {
-        console.log('api', key)
-      }
-      let response = await api.getuser()
+      let response = await api.getUser({})
       assert(response !== null)
     });
+});
+
+describe('#find questions', function() {
+  it('respond with matching records', async function() {
+    let response = await api.getQuestions({})
+    assert(response !== null)
+  });
+});
+
+describe('#parameters', function() {
+  it('respond with page required', async function() {
+    let response = await api.getQuestions({page: 2})
+    assert(response !== null)
+    assert('data' in response, true, 'data is not in response')
+    assert('_meta' in response.data, true, 'meta is not in response data')
+    assert('page' in response.data._meta, true, 'page is not in response data meta')
+    assert(response.data._meta.page, 2, 'It did not get page 2')
+  });
+});
+
+describe('#where builder parameter', function() {
+  it('respond with page required', async function() {
+    let emails = ['gutorc@hotmail.com', 'ziul@ziul.com']
+    let query = {
+      email: {$in: emails }
+    }
+    let response = await api.getUser({where: query})
+    assert(response !== null)
+    assert('data' in response, true, 'data is not in response')
+    assert('_meta' in response.data, true, 'meta is not in response data')
+    assert('total' in response.data._meta, true, 'total is not in response data meta')
+    assert(response.data._meta.otal, 2, 'It did not get two results')
+  });
 });
