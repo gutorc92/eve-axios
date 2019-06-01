c31dd4c828a2ca874f651192a617c522de962453
Luiz Oliveira <ziuloliveira@gmail.com>
2019-06-01 02:13:38 -0300

    fixed the vebose
    Signed-off-by: Luiz Oliveira <ziuloliveira@gmail.com>

--- test/test.user.spec.js
+++ test/test.user.spec.js
@@ -6,8 +6,8 @@ describe('#find users ()', function() {
     it('respond with matching records', async function() {
       let response = await api.getUser({ max: 4})
       assert(response !== null)
-      assert(response.data !== undefined)
-      // it's ending with timeout!
+      assert(response.code !== 'ECONNABORTED', 'aborted with timeout')
+      assert(response.data !== undefined, "didn't get a valid response")
     });
   });
   
@@ -22,9 +22,9 @@ describe('#find users ()', function() {
     it('respond with page required', async function() {
       let response = await api.getQuestions({page: 2})
     assert(response !== null)
-    assert(response.data !== undefined, true, 'data is not in response')
-    assert(response.data._meta !== null, true, 'meta is not in response data')
-    assert('page' in response.data._meta, true, 'page is not in response data meta')
+    assert(response.data !== undefined, 'data is not in response')
+    assert(response.data._meta !== null, 'meta is not in response data')
+    assert('page' in response.data._meta, 'page is not in response data meta')
     assert(response.data._meta.page, 2, 'It did not get page 2')
   });
 });
@@ -37,9 +37,9 @@ describe('#where builder parameter', function() {
     }
     let response = await api.getUser({where: query})
     assert(response !== null)
-    assert('data' in response, true, 'data is not in response')
-    assert('_meta' in response.data, true, 'meta is not in response data')
-    assert('total' in response.data._meta, true, 'total is not in response data meta')
+    assert('data' in response, 'data is not in response')
+    assert('_meta' in response.data, 'meta is not in response data')
+    assert('total' in response.data._meta, 'total is not in response data meta')
     assert(response.data._meta.total, 2, 'It did not get two results')
   });
 });
